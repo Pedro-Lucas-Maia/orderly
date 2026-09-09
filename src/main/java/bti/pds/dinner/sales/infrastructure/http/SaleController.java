ea96214448167e25fc2de5e5348b0332fe34dfb2
Pedro Lucas Maia <pedromaiadepaiva@gmail.com>
2026-09-09 08:11:51 -0300

    refactor: change controller to use new requests and responses

--- src/main/java/bti/pds/dinner/sales/infrastructure/http/SaleController.java
+++ src/main/java/bti/pds/dinner/sales/infrastructure/http/SaleController.java
@@ -1,18 +1,13 @@
 package bti.pds.dinner.sales.infrastructure.http;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import bti.pds.dinner.sales.application.request.CreateSaleRequest;
-import bti.pds.dinner.sales.application.response.SaleResponse;
 import bti.pds.dinner.sales.application.service.SaleService;
+import bti.pds.dinner.sales.infrastructure.http.request.CreateSaleRequest;
+import bti.pds.dinner.sales.infrastructure.http.response.SaleResponse;
 import lombok.AllArgsConstructor;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 
@@ -23,19 +18,22 @@ public class SaleController {
 
     @PostMapping("/api/sales")
     public ResponseEntity<SaleResponse> createSale(@RequestBody CreateSaleRequest request) {
-        SaleResponse response = saleService.execute(request);
+        SaleResponse response = SaleResponse.from(saleService.createSale(CreateSaleRequest.toInput(request)));
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping("/api/sales")
     public List<SaleResponse> listSales() {
-        return saleService.listSales();
+        return saleService.listSales()
+                .stream()
+                .map(SaleResponse::from)
+                .toList();
     }
 
     @GetMapping("/api/sales/{id}")
     public SaleResponse getSaleById(@PathVariable String id) {
-        return saleService.getSaleById(id);
+        return SaleResponse.from(saleService.getSaleById(id));
     }
 
     @PostMapping("/api/sales/{id}/cancel")
