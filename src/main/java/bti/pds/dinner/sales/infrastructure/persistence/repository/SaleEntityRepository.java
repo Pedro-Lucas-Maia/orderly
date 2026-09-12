a872d4dae19e092d2c11d7563110723afcdb52e8
Pedro Maia <pedromaiadepaiva@gmail.com>
2026-09-12 12:05:34 -0300

    fix: change file names to ensure project standards

--- src/main/java/bti/pds/dinner/sales/infrastructure/persistence/repository/SaleEntityRepository.java
+++ src/main/java/bti/pds/dinner/sales/infrastructure/persistence/repository/SaleEntityRepository.java
@@ -1,41 +1,9 @@
-// src/main/java/bti/pds/dinner/sales/infrastructure/persistence/repository/SaleRepositoryImpl.java
 package bti.pds.dinner.sales.infrastructure.persistence.repository;
 
-import bti.pds.dinner.sales.domain.Sale;
-import bti.pds.dinner.sales.domain.SaleID;
-import bti.pds.dinner.sales.domain.SaleRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import bti.pds.dinner.sales.infrastructure.persistence.entity.SaleEntity;
-import org.jspecify.annotations.NonNull;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public class SaleEntityRepository implements SaleRepository {
-
-    private final JpaSaleRepository jpaRepository;
-
-    public SaleEntityRepository(JpaSaleRepository jpaRepository) {
-        this.jpaRepository = jpaRepository;
-    }
-
-    @Override
-    public Sale save(Sale sale) {
-        return SaleEntity.toDomain(jpaRepository.save(SaleEntity.from(sale)));
-    }
-
-    @Override
-    public Optional<Sale> findById(@NonNull SaleID id) {
-        return jpaRepository.findById(id.uuid().toString())
-                .map(SaleEntity::toDomain);
-    }
-
-    @Override
-    public List<Sale> findAll() {
-        return jpaRepository.findAll()
-                .stream()
-                .map(SaleEntity::toDomain)
-                .toList();
-    }
+public interface SaleEntityRepository extends JpaRepository<SaleEntity, String>{
+    
 }
