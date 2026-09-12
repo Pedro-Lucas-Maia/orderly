4bbad6796aba6a60f140cd4c37bb58042c3fe1fa
Pedro Maia <pedromaiadepaiva@gmail.com>
2026-09-12 12:13:51 -0300

    fix: change output to minimize coupling between domain and DTO's

--- src/main/java/bti/pds/dinner/sales/application/output/SaleOutput.java
+++ src/main/java/bti/pds/dinner/sales/application/output/SaleOutput.java
@@ -1,8 +1,6 @@
 package bti.pds.dinner.sales.application.output;
 
-import bti.pds.dinner.sales.domain.Sale;
 import bti.pds.dinner.sales.domain.SaleStatus;
-import org.jspecify.annotations.NonNull;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -16,17 +14,4 @@ public record SaleOutput(
         String observation,
         List<SaleItemOutput> items
 ) {
-    public static SaleOutput from(@NonNull Sale sale, LocalDateTime referenceDate) {
-        return new SaleOutput(
-                sale.getId().toString(),
-                referenceDate,
-                sale.getStatus(),
-                sale.calculateTotal(),
-                sale.getObservation(),
-                sale.getItems()
-                        .stream()
-                        .map(SaleItemOutput::from)
-                        .toList()
-        );
-    }
 }
